d6185998fd61b3dc4bc5f2369eb94661f466186b
antu <shubhashishkar@gmail.com>
2025-01-16 12:27:16 -0800

    [add] pull based workload balance code added

--- ParallelEnumeration.h
+++ ParallelEnumeration.h
@@ -15,8 +15,12 @@ public:
     static size_t* exploreWithEvenWorkloadEstimation(const Graph *data_graph, const Graph *query_graph, ui **candidates, ui *candidates_count, ui *order,
                                                        TreeNode *&tree, size_t* est_work_array, size_t thread_output_limit_num, size_t &call_count);
 
+    static void exploreWithPullBasedLoadBalancing(const Graph *data_graph, const Graph *query_graph, ui **candidates, ui *candidates_count, ui *order,
+                                                       TreeNode *&tree, size_t* est_work_array, size_t thread_output_limit_num, size_t &call_count);
+
     static void compareBetweenEstimationAndRealCount(const Graph *data_graph, const Graph *query_graph, ui **candidates, ui *candidates_count, ui *order,
                                                        TreeNode *&tree, size_t* est_work_array, size_t thread_output_limit_num, size_t &call_count);
+
 };
 
 
